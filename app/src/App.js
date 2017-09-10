34fec6843d8683e260577bc617951d9f1dab1ca0
Jerome Krieg <miaoulafrite@gmail.com>
2017-09-10 17:40:27 +0100

    Basic react and flask integration.

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.getUsers()
+  }
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { console.log(res.data.data.users); })
+    .catch((err) => { console.log(err); })
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <br/>
+              <h1>All Users</h1>
+              <hr/><br/>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }

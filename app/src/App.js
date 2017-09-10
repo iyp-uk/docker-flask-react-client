06c713c351ff23a953b8e08abf09f20a1272e096
Jerome Krieg <miaoulafrite@gmail.com>
2017-09-10 18:48:21 +0100

    Use state and create component for UsersList.

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import UsersList from './components/UsersList'
 
 class App extends Component {
   constructor() {
     super()
-    this.getUsers()
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    this.getUsers();
   }
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-    .then((res) => { console.log(res.data.data.users); })
+    .then((res) => { this.setState({users: res.data.data.users}) })
     .catch((err) => { console.log(err); })
   }
   render() {
@@ -21,6 +27,7 @@ class App extends Component {
               <br/>
               <h1>All Users</h1>
               <hr/><br/>
+              <UsersList users={this.state.users} />
             </div>
           </div>
         </div>
